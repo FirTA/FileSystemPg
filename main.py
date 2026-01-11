4ad97dbdb8ca2d14fed39941f16cc61a55d28df4
Firdauz Tri Anggoro <86070626+FirTA@users.noreply.github.com>
2026-01-12 06:25:01 +0700

    Refactor database connection to use environment variables for configuration

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+
+def list_dir(curr, path):
+    curr.execute(f"SELECT pg_ls_dir('{path}')")
+    return curr.fetchone()
+
 
 if __name__ == '__main__':
+    load_dotenv()
     print('test')
     conn = psycopg2.connect(
-        dbname='mcgdata',
-        user='mcgdata',
-        password='mcgdata',
-        host='172.31.93.221',
-        port='5321'
+        dbname=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT')
     )
+
+    
     with conn.cursor() as cur:
-        cur.execute('SELECT version()')
-        db_version = cur.fetchone()
-        print(db_version)
-        cur.execute('SELECT pg_ls_dir(''/'')')
-        list_dir = cur.fetchone()
-        print(list_dir)
+        while True:
+            inp = input('root path : ')  
+            db_fetch = list_dir(cur, f'{inp}')
+            print(db_fetch)
